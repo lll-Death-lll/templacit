620f9d33332015298137736258b61fb21b0c0d44
Roman Avdeev <RomanAAvdeev@proton.me>
2024-10-26 17:16:53 +0400

    Move items list to txt file

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,76 @@
-use std::{fs, io::Write, path::PathBuf};
+use std::{env, fs, io::Write, path::PathBuf};
 mod files;
 mod item;
 mod minecraft_item;
 
-use clap::arg;
-use clap::Parser;
-use clap::Subcommand;
+use clap::{arg, command, value_parser, ArgAction};
 use files::*;
 use item::*;
 use minecraft_item::*;
 
-#[derive(Parser, Debug)]
-#[command(
-    version = "0.0.1",
-    about = "Minecraft CIT resourcepack creation",
-    long_about = "A tool for easier creation of minecraft resource packs with CIT"
-)]
-struct Args {
-    /// Name of the new item
-    #[arg(short, long)]
-    name: String,
-
-    #[command(subcommand)]
-    source: ItemSource,
-
-    /// Minecraft items id's you want to replace, separated by comma ','
-    #[arg(short, long, value_delimiter = ',')]
-    items: Vec<String>,
-}
-
-#[derive(Subcommand, Debug)]
-enum ItemSource {
-    Texture {
-        /// Texture of the new item
-        #[arg(short, long, value_name = "FILE")]
-        texture: PathBuf,
-    },
-    Model {
-        /// Model of the new item
-        #[arg(short, long, value_name = "FILE")]
-        model: PathBuf,
-    },
-}
-
-impl<'a> ViewSource<'a> {
-    fn from_item_source(item_source: &'a ItemSource) -> Result<ViewSource<'a>, String> {
-        match item_source {
-            ItemSource::Texture { texture } => ViewSource::new(texture), // Assumes ViewSource::new takes a reference
-            ItemSource::Model { model } => ViewSource::new(model), // Assumes ViewSource::new takes a reference
-        }
-    }
-}
-
 fn main() {
-    let args = Args::parse();
+    let matches = command!()
+        .arg(
+            arg!(-n --name <item_name> "Name of the new item")
+                .required(false)
+                .value_parser(value_parser!(String))
+        )
+        .arg(
+            arg!(-t --texture <texture_file> "Texture of the new item")
+                .required(true)
+                .value_parser(value_parser!(PathBuf))
+                .conflicts_with("model")
+        )
+        .arg(
+            arg!(-m --model <model_file> "Model of the new item")
+                .required(true)
+                .value_parser(value_parser!(PathBuf))
+                .conflicts_with("texture")
+        )
+        .arg(
+            arg!(-i --items <items_list> "Minecraft items id's you want to replace, separated by comma ','")
+                .required(true)
+                .value_parser(value_parser!(String))
+                .action(ArgAction::Append)
+                .value_delimiter(',')
+        )
+        .get_matches();
 
     let item = Item::new();
 
-    let view_source = match ViewSource::from_item_source(&args.source) {
-        Ok(view_source) => view_source,
-        Err(e) => {
-            println!("View source not found: {e}");
-            return;
-        }
+    let view_source = if let Some(texture) = matches.get_one::<PathBuf>("texture") {
+        ViewSource::new(texture)
+    } else if let Some(model) = matches.get_one::<PathBuf>("model") {
+        ViewSource::new(model)
+    } else {
+        Err("How did we get here? No texture or model".to_owned())
+    };
+    if let Err(e) = view_source {
+        println!("Error while looking for a file: {e}");
+        return;
+    }
+    let item = item.source(view_source.expect("View source not found"));
+
+    let item = match matches.get_one::<String>("name") {
+        Some(name) => item.name(name),
+        None => item.name_from_source(),
     };
 
-    let Ok(item) = item.source(view_source).name(&args.name).items(
-        args.items
-            .iter()
-            .filter_map(|item_name| match MinecraftItem::new(item_name) {
-                Ok(item) => return Some(item),
-                Err(e) => {
-                    println!("Error looking for items: {}", e);
-                    return None;
-                }
-            })
-            .collect(),
-    ) else {
-        println!("Error while building item");
+    let mut minecraft_items = vec![];
+    if let Some(items) = matches.get_many::<String>("items") {
+        for item in items {
+            match MinecraftItem::new(item.as_str()) {
+                Ok(i) => minecraft_items.push(i),
+                Err(e) => println!("Error while adding item: {}", e),
+            }
+        }
+    }
+    let item = item.items(minecraft_items);
+    if let Err(e) = item {
+        println!("Error looking for items: {}", e);
         return;
-    };
+    }
+    let item = item.unwrap();
 
     let item = item.build();
 
